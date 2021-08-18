6d6c6a9e8728357099e1b9db51d279d818daa5f1
Suvdaa <hertlauriddle88@gmail.com>
2021-08-18 15:17:35 +0800

    Shoog buulgadag bolgoson

--- app.js
+++ app.js
@@ -10,14 +10,23 @@ var roundScore=0;
 
 // Шоо буухад аль талаараа буухыг хадгалга. 1-6 хүртэл санамсаргүй утга авдаг болгоно
 // <div class="player-name" id="name-0">Player 1</div>
-var dice= Math.floor(Math.random()*6)+1 ; 
+var diceNumber= Math.floor(Math.random()*6)+1 ; 
 // DOM өөрчлөх
-window.document.querySelector('#score-0').textContent=0;document.querySelector('#score-1').textContent=0;
-document.querySelector('#current-0').textContent=0;document.querySelector('#current-1').textContent=0;
+window.document.getElementById('score-0').textContent='0';document.getElementById('score-1').textContent='0';
+document.getElementById('current-0').textContent='0';document.getElementById('current-1').textContent='0';
+
+// Шоогоо буулгадаг болцгооё
+var diceDom=document.querySelector(".dice");
+diceDom.style.display='none';
+document.querySelector(".btn-roll").addEventListener("click",function(){
+    var diceNumber= Math.floor(Math.random()*6)+1 ;
+
+    diceDom.style.display="block";
+    diceDom.src='dice-'+diceNumber+'.png';
+    
+});
 
 
-
-document.querySelector(".dice").style.display='none';
 
 // console.log('Шоо : '+ dice)
-// Шоогоо буулгадаг болцгооё
+// Тоглогчдын ээлжийг солих
